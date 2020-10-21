10881a45a173bee452c3f5d7f7686bf73b1a9734
Chris Jantzen <jantzeca@mail.uc.edu>
2020-10-21 16:38:42 -0400

    ip address

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from os import listdir
+import socket
 from functools import reduce
 
 def main():
+    # Get input files
     myPath = './home/data'
     fileNames = [f for f in listdir(myPath)]
     myFiles = ['{}/{}'.format(myPath, f) for f in fileNames]
@@ -9,6 +11,7 @@ def main():
     def getWordCount(text):
         return len(text.replace('\n', ' ').split())
 
+    # Find Longest File
     wordMax = -1
     maxFile = ''
     for i in myFiles:
@@ -19,13 +22,17 @@ def main():
             wordMax = wordCount
         f.close()
 
-    print('Longest file:', maxFile)
-
+    # Find output file and prepare file names text
     outputFile = './home/output/' + listdir('./home/output')[0]
     fileNamesString = reduce(lambda acc, curr: acc + f'{curr}, ', fileNames, '')[: -2]
-    print(fileNamesString)
+
+    # Get IP Address
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 1))
+    local_ip_address = s.getsockname()[0]
+
     with open(outputFile, 'w') as f:
-        f.write(f'Files: {fileNamesString}\nLongest file: {maxFile} with {wordMax} words.')
+        f.write(f'Files: {fileNamesString}\nLongest file: {maxFile} with {wordMax} words.\nipaddress: {local_ip_address}')
         f.close()
 
 if __name__ == '__main__':
